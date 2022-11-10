74c1ae99b4fafe908116013b85bf8b50790527ec
Tallal <=>
2022-11-10 17:36:10 +0500

    validations added to models

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -3,4 +3,15 @@
 # Stores stats for Pokemon table
 class Stat < ApplicationRecord
   belongs_to :pokemon
+  validates :base_stat, numericality: { only_integer: true }
+  validates :effort, numericality: { only_integer: true }
+  validate :format_of_stat_hash
+
+  def format_of_stat_hash
+    unless stat.is_a?(Hash)
+      errors.add(:stat, "is not a hash")
+      return
+    end
+  end
+
 end
